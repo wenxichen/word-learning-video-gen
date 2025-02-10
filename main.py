4131e77fcdc8ad86f7caa9ac59df3e83e7440ecd
Wenxi Chen <wc2580@columbia.edu>
2025-02-09 22:24:13 -0800

    basic working version

--- main.py
+++ main.py
@@ -10,19 +10,25 @@ from pptx import Presentation
 from pptx.util import Inches, Pt
 import subprocess
 from pydub import AudioSegment
-from moviepy.editor import ImageClip, AudioFileClip
+from moviepy.editor import ImageClip, AudioFileClip, concatenate_videoclips, VideoFileClip
 import logging
+from tqdm import tqdm
 
 load_dotenv()
 
-# Configure logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
+# # Configure logging
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format='%(asctime)s - %(levelname)s - %(message)s'
+# )
+
+
+# load word list from file
+with open("materials/common_3000_eng_words.txt", "r") as file:
+    word_list = file.read().splitlines()
 
 output_dir = os.getcwd() + "/output/"
-
+cache_dir = os.getcwd() + "/cache/"
 # Anthropic
 import anthropic
 anthropic_client = anthropic.Anthropic()
@@ -45,9 +51,15 @@ def generate_word_info(word: str) -> Dict[str, str]:
     system_message: str = "You are a kintergarden teacher. You are given a word and you need to explain it in a way that is easy to understand."
     user_message: str = (f"Can you explain the word {word} in a way that is easy to understand for a 5 year old? "
                 f"Please respond in JSON format with the following two fields: 'definition' and 'example'. "
-                f"The definition should be no more than a couple of sentences explaining the most common definition(s) of the word. "
-                f"The example should be a sentence that uses the word in a way that is easy to understand for a 5 year old. "
-                f"Start the example sentence with something like 'For example, ...', 'Here is an example: ...', or 'An example would be ...'.")
+                f"The definition should be no more than a couple of sentences and no more than 25 words explaining the most common definition(s) of the word. "
+                f"The example should be a sentence and no more than 25 words that uses the word in a way that is easy to understand for a 5 year old. "
+                f"Start the example sentence with something like 'For example, ...', 'Here is an example: ...', or 'An example would be ...'."
+                f"\n\n"
+                f"Here is an example JSON response: "
+                f"{{"
+                f"  'definition': 'A definition of the word', "
+                f"  'example': 'An example sentence using the word'"
+                f"}}")
     
     message: anthropic.Message = anthropic_client.messages.create(
         model="claude-3-5-sonnet-20241022",
@@ -66,11 +78,19 @@ def generate_word_info(word: str) -> Dict[str, str]:
             }
         ]
     )
+    # print(message.content)
 
-    word_info: Dict[str, str] = json.loads(message.content[0].text)
-    word_info["word"] = word
+    try:
+        word_info: Dict[str, str] = json.loads(message.content[0].text)
+        word_info["word"] = word
+        return word_info
+    except:
+        print("Failed to parse JSON response for word: " + word + " message: " + message.content[0].text)
+        # record word and message to file
+        with open(output_dir + "failed_words.txt", "a") as file:
+            file.write(word + " " + message.content[0].text + "\n")
+        return None
 
-    return word_info
 
 def generate_image_from_word_info(word_info: Dict[str, str]) -> BytesIO:
     """
@@ -147,7 +167,7 @@ def generate_slide(word_info: Dict[str, str], image_data: BytesIO) -> str:
     p.text = word_info["example"]
 
     # save pptx file
-    output_file_path = output_dir + 'temp.pptx'
+    output_file_path = cache_dir + 'temp.pptx'
     prs.save(output_file_path)
 
     return output_file_path
@@ -162,17 +182,17 @@ def convert_slide_to_image(slide_path: str) -> str:
     Returns:
         str: The path to the generated image.
     """
-    pdf_path = os.path.join(output_dir, "temp.pdf")
+    pdf_path = cache_dir + "temp.pdf"
 
     # Convert PPTX to PDF using LibreOffice
-    subprocess.run(["/Applications/LibreOffice.app/Contents/MacOS/soffice", "--headless", "--convert-to", "pdf", slide_path, "--outdir", output_dir], check=True)
+    subprocess.run(["/Applications/LibreOffice.app/Contents/MacOS/soffice", "--headless", "--convert-to", "pdf", slide_path, "--outdir", cache_dir], check=True)
 
     # Convert PDF to PNG images using pdftoppm (part of poppler-utils)
-    subprocess.run(["pdftoppm", "-png", "-singlefile", pdf_path, os.path.join(output_dir, "temp")], check=True)
+    subprocess.run(["pdftoppm", "-png", "-singlefile", pdf_path, cache_dir + "temp"], check=True)
 
     os.remove(pdf_path)
 
-    return os.path.join(output_dir, "temp.png")
+    return cache_dir + "temp.png"
 
 
 def combine_mp3s(file_paths: List[str], output_path: str, pause_duration: int = 2000) -> str:
@@ -191,8 +211,7 @@ def combine_mp3s(file_paths: List[str], output_path: str, pause_duration: int = 
     for i, file_path in enumerate(file_paths):
         audio = AudioSegment.from_mp3(file_path)
         combined_audio += audio
-        if i < len(file_paths) - 1:  # Add pause except after the last file
-            combined_audio += pause
+        combined_audio += pause
     combined_audio.export(output_path, format="mp3")
 
 
@@ -206,7 +225,7 @@ def generate_audio_file(word_info: Dict[str, str]) -> str:
     Returns:
         str: The path to the generated audio file.
     """
-    word_speech_file_path = output_dir + "word_speech.mp3"  
+    word_speech_file_path = cache_dir + "word_speech.mp3"  
     with openai_client.audio.speech.with_streaming_response.create(
         model="tts-1",
         voice="shimmer",
@@ -214,7 +233,7 @@ def generate_audio_file(word_info: Dict[str, str]) -> str:
     ) as response:
         response.stream_to_file(word_speech_file_path)
 
-    word_definition_speech_file_path = output_dir + "word_definition_speech.mp3"    
+    word_definition_speech_file_path = cache_dir + "word_definition_speech.mp3"    
     with openai_client.audio.speech.with_streaming_response.create(
         model="tts-1",
         voice="shimmer",
@@ -222,7 +241,7 @@ def generate_audio_file(word_info: Dict[str, str]) -> str:
     ) as response:
         response.stream_to_file(word_definition_speech_file_path)
 
-    word_example_speech_file_path = output_dir + "word_example_speech.mp3"
+    word_example_speech_file_path = cache_dir + "word_example_speech.mp3"
     with openai_client.audio.speech.with_streaming_response.create(
         model="tts-1",
         voice="nova",
@@ -231,8 +250,12 @@ def generate_audio_file(word_info: Dict[str, str]) -> str:
         response.stream_to_file(word_example_speech_file_path)
 
     file_paths = [word_speech_file_path, word_definition_speech_file_path, word_example_speech_file_path]
-    output_path = output_dir + "combined_audio.mp3"
+    output_path = cache_dir + "combined_audio.mp3"
     combine_mp3s(file_paths, output_path)
+
+    os.remove(word_speech_file_path)
+    os.remove(word_definition_speech_file_path)
+    os.remove(word_example_speech_file_path)
 
     return output_path
 
@@ -252,32 +275,91 @@ def generate_video_file(image_path: str, audio_path: str, output_path: str) -> s
     video_clip = image_clip.set_audio(audio_clip)
     video_clip.write_videofile(output_path, fps=24)
 
-if __name__ == "__main__":
-    logging.info("Starting video generation process...")
+
+def generate_video_for_word(word: str, output_path: str):
+    logging.debug("Starting video generation process...")
     
-    logging.info("Generating word information...")
-    word_info = generate_word_info("apple")
-    logging.info(f"Generated word info for '{word_info['word']}'")
+    logging.debug("Generating word information...")
+    word_info = generate_word_info(word)
+    if word_info is None:
+        logging.debug("Failed to generate word info for word: " + word)
+        return False
+    logging.debug(f"Generated word info for '{word_info['word']}'")
     
-    logging.info("Generating image from word info...")
+    logging.debug("Generating image from word info...")
     image_data = generate_image_from_word_info(word_info)
-    logging.info("Image generated successfully")
+    logging.debug("Image generated successfully")
     
-    logging.info("Creating PowerPoint slide...")
+    logging.debug("Creating PowerPoint slide...")
     slide_path = generate_slide(word_info, image_data)
-    logging.info(f"Slide created at {slide_path}")
+    logging.debug(f"Slide created at {slide_path}")
     
-    logging.info("Converting slide to image...")
+    logging.debug("Converting slide to image...")
     image_path = convert_slide_to_image(slide_path)
-    logging.info(f"Slide converted to image at {image_path}")
+    logging.debug(f"Slide converted to image at {image_path}")
     
-    logging.info("Generating audio file...")
+    logging.debug("Generating audio file...")
     audio_path = generate_audio_file(word_info)
-    logging.info(f"Audio generated at {audio_path}")
+    logging.debug(f"Audio generated at {audio_path}")
     
-    logging.info("Creating final video...")
-    video_path = output_dir + "video.mp4"
-    generate_video_file(image_path, audio_path, video_path)
-    logging.info(f"Video created successfully at {video_path}")
+    logging.debug("Creating final video...")
+    generate_video_file(image_path, audio_path, output_path)
+    logging.debug(f"Video created successfully at {output_path}")
     
-    logging.info("Process completed!")
+    logging.debug("Deleting temporary files...")
+    os.remove(slide_path)
+    os.remove(image_path)
+    os.remove(audio_path)
+    logging.debug("Temporary files deleted successfully")
+
+    logging.debug("Process completed!")
+
+    return True
+
+def combine_videos(video_paths: List[str], output_path: str):
+    """
+    Combine multiple video files into one.
+
+    Args:
+        video_paths (List[str]): A list of file paths to the video files to combine.
+        output_path (str): The path to save the combined video file.
+    """
+    video_clips = [VideoFileClip(path) for path in video_paths]
+    final_clip = concatenate_videoclips(video_clips, method="compose")
+    final_clip.write_videofile(output_path, codec="libx264")
+
+def combine_videos_from_cache_files(video_ids: List[str], output_path: str):
+    """
+    Combine multiple video files from cache files into one.
+    """
+    video_paths = []
+    for video_to_include in video_ids:
+        for file_path in os.listdir(cache_dir):
+            file_abs_path = cache_dir + file_path
+            if os.path.isfile(file_abs_path) and file_path.startswith(video_to_include+'_'):
+                video_paths.append(file_abs_path)
+    combine_videos(video_paths, output_path)
+
+
+if __name__ == "__main__":
+    # # count is used for counting and skipping
+    # count = 22
+    # videos_to_include = ["21", "22"] 
+    # video_paths = []
+    # for video_to_include in videos_to_include:
+    #     for file_path in os.listdir(cache_dir):
+    #         file_abs_path = cache_dir + file_path
+    #         if os.path.isfile(file_abs_path) and file_path.startswith(video_to_include+'_'):
+    #             video_paths.append(file_abs_path)
+
+    # for i, word in enumerate(tqdm(word_list[count:100])):
+    #     video_path = cache_dir + f"{count+1:02d}_" + word + ".mp4"
+    #     success = generate_video_for_word(word, video_path)
+    #     count += 1
+    #     if success:
+    #         video_paths.append(video_path)
+    #     if count % 10 == 0:
+    #         combine_videos(video_paths, output_dir + f"combined_{count-9}-{count}.mp4")
+    #         video_paths = []
+
+    combine_videos_from_cache_files(["11", "12", "13", "14", "15", "16", "17", "18", "19", "20"], output_dir + "combined_11-20.mp4")
